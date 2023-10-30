9ef5f116199f8da1a67d1d31a8e02f8c4c426273
abel-tefera <abeltefera16@gmail.com>
2023-10-30 15:30:48 +0300

    React TS starter done

--- react-ts-sample/src/index.tsx
+++ react-ts-sample/src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom";
 import GuestList from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+// import UserSearch from "./state/UserSearch";
+import UserSearchWithRef from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
       <GuestList></GuestList>
-      <UserSearch></UserSearch>
+      <UserSearchWithRef></UserSearchWithRef>
     </div>
   );
 };
