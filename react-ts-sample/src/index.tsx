cc296cba29a98d911366ab46ff955c8ec72877ae
abel-tefera <abeltefera16@gmail.com>
2023-10-30 14:34:44 +0300

    Wip

--- react-ts-sample/src/index.tsx
+++ react-ts-sample/src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <GuestList></GuestList>
+      <UserSearch></UserSearch>
     </div>
   );
 };
